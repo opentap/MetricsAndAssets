a98d9860423069225ffe90bc2a8a740c2d981bae
Asger Iversen <20689829+AsgerIversen@users.noreply.github.com>
2025-01-22 11:52:11 +0100

    Experimenting with an IAssetDetails interface as a way for an instrument implementation to provide more details on an asset

--- OpenTap.Metrics/AssetDiscovery/AssetDiscoveryManager.cs
+++ OpenTap.Metrics/AssetDiscovery/AssetDiscoveryManager.cs
@@ -34,6 +34,29 @@ public static class AssetDiscoveryManager
         return assets;
     }
 
+    /// <summary>
+    /// Given a generic asset as returned by DiscoverAllAssets, this method will return a more specialized asset type with additional details.
+    /// This relies on being able to find IAssetDetails implementations that can provide the additional details for the given asset manufavturer and model.
+    public statis DiscoveredAsset GetAssetDetails(DiscoveredAsset asset)
+    {
+        var detailsProviders = TypeData.GetDerivedTypes<IAssetDetails>().ToList();
+        foreach (var provider in detailsProviders)
+        {
+            try
+            {
+                if (provider.Get(asset))
+                {
+                    return provider.GetAssetDetails(asset);
+                }
+            }
+            catch (Exception ex)
+            {
+                log.Error($"Error while getting details for asset {asset.Id} from {provider.GetType().Name}: {ex.Message}");
+            }
+        }
+        return null;
+    }
+
     private static IEnumerable<IAssetDiscovery> _assetDiscoveryProviders;
     private static IEnumerable<IAssetDiscovery> GetAssetDiscoveryProviders()
     {
@@ -45,6 +68,19 @@ public static class AssetDiscoveryManager
                 .ToList();
         }
         return _assetDiscoveryProviders;
+    }
+
+    private static IEnumerable<IAssetDetails> _assetDetailsProviders;
+    private static IEnumerable<IAssetDetails> GetAssetDetailsProviders()
+    {
+        if (_assetDetailsProviders == null)
+        {
+            _assetDetailsProviders = TypeData.GetDerivedTypes<IAssetDetails>().Where(x => x.CanCreateInstance)
+                .Select(x => x.CreateInstance() as IAssetDetails)
+                .OrderByDescending(x => x.Priority)  // Higher (numeric value) priority should be used first.
+                .ToList();
+        }
+        return _assetDetailsProviders;
     }
 
     // public static void PushDiscoveredAssets(DiscoveredAsset asset)

09454ec50c7d14748260367a2a99a04a9eba6a43
Asger Iversen <20689829+AsgerIversen@users.noreply.github.com>
2025-01-22 12:48:50 +0100

    Added AssetDiscoverySettings

---
+++ OpenTap.Metrics/AssetDiscovery/AssetDiscoverySettings.cs
@@ -0,0 +1,24 @@
+using System.Linq;
+
+namespace OpenTap.Metrics.AssetDiscovery;
+
+[Display("Asset Discovery", "List of asset discovery impelmentations to use.")]
+public class AssetDiscoverySettings : ComponentSettingsList<AssetDiscoverySettings, IAssetDiscovery>
+{
+
+    public AssetDiscoverySettings()
+    {
+        // By default, add all available asset discovery providers.
+        // this will be overridden by the deserializer, if we have a saved configuration.
+        var assetDiscoveryProviders = TypeData.GetDerivedTypes<IAssetDiscovery>().Where(x => x.CanCreateInstance)
+            .Select(x => x.CreateInstance() as IAssetDiscovery);
+        foreach (var provider in assetDiscoveryProviders)
+        {
+            Add(provider);
+        }
+    }
+
+}
+
+
+

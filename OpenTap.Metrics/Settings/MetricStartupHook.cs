dfcfacc2c63134a3c0ac8524eb908b25590cb40e
Alexander Larsen <alexander.larsen@keysight.com>
2025-02-04 12:13:05 +0100

    Make metrics always appear browsable in package xml

--- OpenTap.Metrics/Settings/MetricStartupHook.cs
+++ OpenTap.Metrics/Settings/MetricStartupHook.cs
@@ -1,3 +1,9 @@
+using System;
+using System.IO;
+using System.Linq;
+using System.Text;
+using OpenTap.Package;
+
 namespace OpenTap.Metrics.Settings;
 
 public class MetricStartupHook : IStartupInfo
@@ -6,4 +12,27 @@ public class MetricStartupHook : IStartupInfo
     {
         MetricInfoTypeDataSearcher.InitialInfos = [.. MetricManager.GetMetricInfos(), ..MetricManager.GetAbstractMetricInfos()];
     }
+}
+
+public class EnsureMetricsBrowsableAction : ICustomPackageAction
+{
+    public int Order() => 999;
+    
+
+    public bool Execute(PackageDef package, CustomPackageActionArgs customActionArgs)
+    {
+        foreach (var file in package.Files)
+        {
+            foreach (var plugin in file.Plugins)
+            {
+                if (plugin.Name.StartsWith(MetricInfoTypeData.MetricTypePrefix))
+                {
+                    plugin.Browsable = true;
+                }
+            }
+        }
+        return true;
+    }
+
+    public PackageActionStage ActionStage => PackageActionStage.Create;
 }

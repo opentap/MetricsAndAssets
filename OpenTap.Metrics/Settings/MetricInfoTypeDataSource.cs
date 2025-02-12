3192539d1acdc5fbb7f649beedf70238e71254a1
Alexander Larsen <alexander.larsen@keysight.com>
2025-02-12 14:08:59 +0100

    Prune duplicate metric specifiers when building list of metrics in an assembly (#77)

--- OpenTap.Metrics/Settings/MetricInfoTypeDataSource.cs
+++ OpenTap.Metrics/Settings/MetricInfoTypeDataSource.cs
@@ -1,11 +1,13 @@
+using System.Collections.Concurrent;
 using System.Collections.Generic;
 using System.Linq;
+using System.Reflection;
 using OpenTap.Package;
 
 namespace OpenTap.Metrics.Settings;
 
 class MetricInfoTypeDataSource : ITypeDataSource
-{ 
+{
     public static bool TryFromTypeData(ITypeData td, out MetricInfoTypeDataSource src)
     {
         src = null;
@@ -13,7 +15,7 @@ class MetricInfoTypeDataSource : ITypeDataSource
         // Because MetricSpecifiers are ambiguous by design (they are intended to merge similar metrics!!)
         // this operation is inherently ambiguous. If a metric is available from different plugins, we will have to 
         // just guess. 
-        
+
         // 1. Find all sources
         var allSources = TypeData.GetDerivedTypes<IMetricSource>().ToArray();
         // 2. Filter the sources list down to the set of sources that provide this metric
@@ -22,20 +24,29 @@ class MetricInfoTypeDataSource : ITypeDataSource
         // 3. Prefer sources that do not come from a package, if possible
         // If we are currently creating a package, then a source which does not come from a package likely belongs
         // to the package we are currently creating.
-        var preferredSource = metricSources.FirstOrDefault(x => Installation.Current.FindPackageContainingType(x) == null) ??
-                     metricSources.FirstOrDefault();
+        var preferredSource =
+            metricSources.FirstOrDefault(x => Installation.Current.FindPackageContainingType(x) == null) ??
+            metricSources.FirstOrDefault();
         if (preferredSource == null)
             return false;
         var std = preferredSource.AsTypeData();
         if (std?.Assembly == null) return false;
         // 4. Find all sources which originale from the same assembly as this source
-        var relatedSources = allSources.Where(x => x.AsTypeData()?.Assembly == std.Assembly)
-            .SelectMany(x => x.GetMetricSpecifiers())
-            .Select(MetricInfoTypeData.FromMetricSpecifier)
-            .ToArray();
-        src = new MetricInfoTypeDataSource(std.Assembly.Name, std.Assembly.Location, relatedSources);
+        src = _cache.GetOrAdd(std.Assembly.Load(),
+            _ =>
+            {
+                var relatedSources = allSources.Where(x => x.AsTypeData()?.Assembly == std.Assembly)
+                    .SelectMany(x => x.GetMetricSpecifiers())
+                    .Distinct()
+                    .Select(MetricInfoTypeData.FromMetricSpecifier)
+                    .ToArray();
+                return new MetricInfoTypeDataSource(std.Assembly.Name, std.Assembly.Location, relatedSources);
+            });
         return true;
     }
+
+    private static readonly ConcurrentDictionary<Assembly, MetricInfoTypeDataSource> _cache = new();
+
     private MetricInfoTypeDataSource(string name, string location, IEnumerable<ITypeData> types)
     {
         Name = name;

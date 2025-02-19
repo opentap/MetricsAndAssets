579cec72f8790956712e92e5d2a572b974df9952
Asger <20689829+AsgerIversen@users.noreply.github.com>
2025-02-19 13:56:41 +0100

    Support --server-url Runner argument (#92)
    * Added a bunch of NATS reconnect options to try to keep the endpoint extensions connection alive
    
    * Check for --nats-server to make sure we connect to the right NATS leaf server

--- OpenTap.Metrics.Nats/RunnerExtension.cs
+++ OpenTap.Metrics.Nats/RunnerExtension.cs
@@ -1,4 +1,5 @@
 using System;
+using System.Linq;
 using System.Text;
 using NATS.Client;
 using Newtonsoft.Json;
@@ -10,11 +11,12 @@ namespace OpenTap.Metrics.Nats
     /// </summary>
     public class RunnerExtension
     {
-        private readonly IConnection connection;
+        private IConnection connection;
         private readonly string runnerId;
         private readonly Guid? sessionId;
         private readonly string baseSubject;
 
+        private static readonly TraceSource _log = Log.CreateSource("Runner Extension");
         private static RunnerExtension instance;
         internal static RunnerExtension Instance
         {
@@ -40,9 +42,29 @@ namespace OpenTap.Metrics.Nats
             }
             sessionId = Guid.Parse(sessionIdVar);
 
+            // The runner secretly allows the user to change the server URL by passing the --server-url command line argument. 
+            // We should check for that to make sure we connect to the correct server.
+            // This is mostly for internal use, as that command line argument is not documented.
+            var clargs = Environment.GetCommandLineArgs();
+            if (clargs.Contains("--nats-server") && clargs.Length > Array.IndexOf(clargs, "--nats-server") + 1)
+            {
+                DefaultServer = clargs[Array.IndexOf(clargs, "--nats-server") + 1];
+            }
+
             var options = ConnectionFactory.GetDefaultOptions();
             options.Servers = new string[] { DefaultServer };
+            options.Name = "Runner Extensions";
+            options.MaxReconnect = Options.ReconnectForever;
+            options.ReconnectWait = 1000;
+            options.AllowReconnect = true;
+            options.PingInterval = 45000;
+            options.Timeout = 5000;
+            options.DisconnectedEventHandler += (sender, args) => { _log.Debug($"NATS connection disconnected"); };
+            options.AsyncErrorEventHandler += (sender, args) => { _log.Info("NATS connection async error: {error}", args.Error); };
+            options.ReconnectedEventHandler = (sender, args) => { _log.Info($"NATS connection reconnected"); };
+
             connection = new ConnectionFactory().CreateConnection(options);
+            _log.Debug($"NATS connected to {DefaultServer}");
             runnerId = connection.ServerInfo.ServerName;
             baseSubject = $"OpenTap.Runner.{runnerId}.Session.{sessionId}";
         }

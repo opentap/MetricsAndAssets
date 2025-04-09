79c47cfce3938aa05c30c847936d44dccef7284a
Alexander Larsen <alexander.larsen@keysight.com>
2025-04-09 15:31:55 +0200

    Update metric kind scan to also include IResource types

--- OpenTap.Metrics/Settings/MetricMemberHelpers.cs
+++ OpenTap.Metrics/Settings/MetricMemberHelpers.cs
@@ -4,7 +4,7 @@ using System.Linq;
 
 namespace OpenTap.Metrics.Settings;
 
-internal static class ExtensionMethods
+internal static class MetricMemberHelpers
 {
     public static int IndexOf<T>(this T[] arr, T elem)
     {
@@ -17,6 +17,12 @@ internal static class ExtensionMethods
     public static IEnumerable<IMemberData> GetMetricMembers(this ITypeData td) =>
         td.GetMembers().Where(mem => mem.HasAttribute<MetricAttribute>());
 
+    public static IEnumerable<ITypeData> GetAllMetricSources() =>
+        TypeData.GetDerivedTypes<IMetricSource>()
+            .Concat(TypeData.GetDerivedTypes<IResource>())
+            .Where(x => x.CanCreateInstance);
+
+    public static IEnumerable<IMemberData> GetAllMetricMembers() => GetAllMetricSources().SelectMany(x => x.GetMetricMembers()); 
     public static IEnumerable<MetricSpecifier> GetMetricSpecifiers(this ITypeData td) =>
         td.GetMetricMembers().Select(x => new MetricSpecifier(x));
 }

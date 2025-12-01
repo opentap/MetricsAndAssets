2d464645b537bc6401a400ff59ca07216b4bdcfd
Alexander Larsen <alexander.larsen@keysight.com>
2025-12-01 15:03:13 +0100

    fix: instrument metrics not added

--- OpenTap.Metrics/Settings/MetricsSettings.cs
+++ OpenTap.Metrics/Settings/MetricsSettings.cs
@@ -16,18 +16,29 @@ public class MetricsSettings : ComponentSettingsList<MetricsSettings, IMetricsSe
         AddDefaultMetrics();
     }
 
+    // Use statically discoverable metrics for adding default metric. This is not related to any deadlock issues,
+    // but rather it is required because we need to add DefaultMetrics from e.g. resources which are not configured on the bench.
+    private MetricSpecifier[] GetStaticMetricInfos() => 
+        [.. MetricMemberHelpers.GetAllMetricSources().SelectMany(MetricMemberHelpers.GetMetricSpecifiers).Distinct()];
+
     private void AddDefaultMetrics()
     {
+        bool anyAdded = false;
         // Ensure duts and instruments are both loaded. This is needed because MetricManager only looks at the cached values.
         var _1 = InstrumentSettings.Current;
         var _2 = DutSettings.Current;
         var existing = this.ToArray();
         var settings = TypeData.GetDerivedTypes<IMetricsSettingsItem>();
-        foreach (var s in settings.OfType<MetricInfoTypeData>())
+        foreach (var s in GetStaticMetricInfos())
         {
-            if (s.Specifier.DefaultEnabled == false) continue;
-            if (existing.Any(x => s.Specifier.Equals((x as MetricsSettingsItem)?.Specifier))) continue;
-            Add(s.CreateInstance() as IMetricsSettingsItem);
+            if (s.DefaultEnabled == false) continue;
+            if (existing.Any(x => s.Equals((x as MetricsSettingsItem)?.Specifier))) continue;
+            Add(new MetricsSettingsItem(s));
+            anyAdded = true;
         }
+
+        // Save the settings. This is needed to ensure metrics settings are persisted if
+        // the plugin providing some metric is removed.
+        if (anyAdded) Save();
     }
 }

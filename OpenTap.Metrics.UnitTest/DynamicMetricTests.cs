ac5ce111dc9d70ebca7a63a420a944b0f6970b33
Alexander Larsen <alexander.larsen@keysight.com>
2025-12-01 13:00:55 +0100

    add unittest demonstrating deadlock

--- OpenTap.Metrics.UnitTest/DynamicMetricTests.cs
+++ OpenTap.Metrics.UnitTest/DynamicMetricTests.cs
@@ -4,8 +4,10 @@
 // file, you can obtain one at http://mozilla.org/MPL/2.0/.
 using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using NUnit.Framework;
+using OpenTap.Metrics.Settings;
 
 namespace OpenTap.Metrics.UnitTest;
 
@@ -76,9 +78,11 @@ public class DynamicMetricTests
     public class DynamicMetricListener : IMetricListener
     {
         public object LastMetric { get; private set; }
+
         public void OnPushMetric(IMetric table)
         {
-            if (table.Info.MetricFullName is "Dynamic Metric Test \\ Pusher" or "Dynamic Nullable Metric Test \\ Pusher")
+            if (table.Info.MetricFullName is "Dynamic Metric Test \\ Pusher"
+                or "Dynamic Nullable Metric Test \\ Pusher")
                 LastMetric = table.Value;
         }
     }
@@ -89,11 +93,13 @@ public class DynamicMetricTests
         MetricManager.Reset();
         using var session = Session.Create(SessionOptions.OverlayComponentSettings);
         MetricInfo result = null;
+
         void onNewMetric(MetricCreatedEventArgs args)
         {
             MetricManager.OnMetricCreated -= onNewMetric;
             result = args.Metric;
         }
+
         var provider = new DynamicMetricProvider();
         MetricManager.OnMetricCreated += onNewMetric;
         var created = MetricManager.CreatePushMetric<double>(provider, "test", "group");
@@ -133,11 +139,13 @@ public class DynamicMetricTests
         MetricManager.Reset();
         using var session = Session.Create(SessionOptions.OverlayComponentSettings);
         MetricInfo result = null;
+
         void onNewMetric(MetricCreatedEventArgs args)
         {
             MetricManager.OnMetricCreated -= onNewMetric;
             result = args.Metric;
         }
+
         var provider = new DynamicNullableMetricProvider();
         MetricManager.OnMetricCreated += onNewMetric;
         var created = MetricManager.CreatePushMetric<double?>(provider, "test", "group");
@@ -178,7 +186,12 @@ public class DynamicMetricTests
         MetricManager.Reset();
         var provider = new DynamicNullableMetricProvider();
         MetricInfo result = null;
-        void onMetricAvailabilityChanged(MetricAvailabilityChangedEventsArgs args) { result = args.Metric; }
+
+        void onMetricAvailabilityChanged(MetricAvailabilityChangedEventsArgs args)
+        {
+            result = args.Metric;
+        }
+
         MetricManager.OnMetricAvailabilityChanged += onMetricAvailabilityChanged;
 
         provider.PushDouble(value);
@@ -193,8 +206,97 @@ public class DynamicMetricTests
     {
         var provider = new DynamicNullableMetricProvider();
 
-        var ex = Assert.Throws<ArgumentException>(() => MetricManager.UpdateAvailability(provider.PollMetric, isAvailable));
+        var ex = Assert.Throws<ArgumentException>(() =>
+            MetricManager.UpdateAvailability(provider.PollMetric, isAvailable));
 
         Assert.That(ex.Message, Is.EqualTo("Cannot update availability of a poll metric. (Parameter 'metric')"));
+    }
+
+    [Test]
+    public void DeadlockTest_1()
+    {
+        using MemoryStream ms =
+            new(
+                """<?xml version="1.0" encoding="utf-8"?><MetricsSettings type="OpenTap.Metrics.Settings.MetricsSettings"></MetricsSettings>"""u8
+                    .ToArray());
+        ComponentSettings.SetCurrent(ms, out IEnumerable<XmlError> errors);
+        var metrics = MetricsSettings.Current.ToArray();
+
+        Assert.That(errors, Is.Empty);
+        // Ensure default metrics were added
+        Assert.That(metrics.Length, Is.GreaterThan(0));
+        var defaultPollMetric = MetricManager.GetMetricInfos().FirstOrDefault(m => m.Name == "Default Poll Metric");
+        Assert.That(defaultPollMetric, Is.Not.Null);
+        var spec = new MetricSpecifier(defaultPollMetric.Member);
+        Assert.That(metrics.Select(x => (x as MetricsSettingsItem)?.Specifier), Contains.Item(spec));
+    }
+
+    [TestCase(true)]
+    [TestCase(false)]
+    public void DeadlockTest_2(bool swap)
+    {
+        var engineSettings = @"<?xml version=""1.0"" encoding=""utf-8""?>
+<EngineSettings type=""OpenTap.EngineSettings"">
+  <SessionLogPath>SessionLogs/SessionLog &lt;Date&gt;.txt</SessionLogPath>
+  <PromptForMetaData>false</PromptForMetaData>
+  <AbortTestPlan>Step_Error</AbortTestPlan>
+  <SummaryTestStepLimit>10000</SummaryTestStepLimit>
+  <OperatorName Metadata=""Name"">Unknown</OperatorName>
+  <StationName Metadata=""Station"">TestPlan Station</StationName>
+  <ResultLatencyLimit>3</ResultLatencyLimit>
+  <LogTimestamper type=""OpenTap.Diagnostic.AccurateStamper""></LogTimestamper>
+  <ResourceManagerType type=""OpenTap.ResourceTaskManager"">
+    <StaticResources />
+    <EnabledSteps />
+  </ResourceManagerType>
+  <LanguageString>Neutral</LanguageString>
+</EngineSettings>"u8.ToArray();
+        var metricSettings = @"<?xml version=""1.0"" encoding=""utf-8""?>
+<MetricsSettings type=""OpenTap.Metrics.Settings.MetricsSettings"">
+  <MetricsSettingsItem type=""Metric:Process_Memory Usage"">
+    <PollRate>60</PollRate>
+    <Specifier>
+      <Name>Memory Usage</Name>
+      <Group>Process</Group>
+      <Type>Double</Type>
+    </Specifier>
+    <IsEnabled>true</IsEnabled>
+  </MetricsSettingsItem>
+  <MetricsSettingsItem type=""Metric:Process_CPU Usage"">
+    <PollRate>60</PollRate>
+    <Specifier>
+      <Name>CPU Usage</Name>
+      <Group>Process</Group>
+      <Type>Double</Type>
+    </Specifier>
+    <IsEnabled>true</IsEnabled>
+  </MetricsSettingsItem>
+  <MetricsSettingsItem type=""Metric:System_Available Memory"">
+    <PollRate>60</PollRate>
+    <Specifier>
+      <Name>Available Memory</Name>
+      <Group>System</Group>
+      <Type>Double</Type>
+    </Specifier>
+    <IsEnabled>true</IsEnabled>
+  </MetricsSettingsItem>
+  <MetricsSettingsItem type=""Metric:System_Available Disk Space"">
+    <PollRate>60</PollRate>
+    <Specifier>
+      <Name>Available Disk Space</Name>
+      <Group>System</Group>
+      <Type>Double</Type>
+    </Specifier>
+    <IsEnabled>true</IsEnabled>
+  </MetricsSettingsItem>
+</MetricsSettings>"u8.ToArray();
+        
+        ComponentSettings.SetCurrent(new MemoryStream(swap ? metricSettings : engineSettings), out IEnumerable<XmlError> errors1);
+        ComponentSettings.SetCurrent(new MemoryStream(swap ? engineSettings : metricSettings), out IEnumerable<XmlError> errors2);
+        
+        var metrics = MetricsSettings.Current.ToArray();
+        Assert.That(metrics.Length, Is.GreaterThan(0));
+        Assert.That(errors1, Is.Empty);
+        Assert.That(errors2, Is.Empty);
     }
 }
